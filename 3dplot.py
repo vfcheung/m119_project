67a5c7402cde233e7236f4bc73e4d7bb6df44753
Justin Chang <jchang0916@ucla.edu>
2019-05-28 15:59:31 -0700

    run this locally on your (not pi, but pc)

--- 3dplot.py
+++ 3dplot.py
@@ -1,9 +1,16 @@
+#run this from machine pc 
+
+from multiprocessing.connection import Listener, Client
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
+import string
+from ast import literal_eval
 
-fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
+
 
 def get_arrow(theta,ys1,ys2,ys3):
     accelero=[1,23,10]
@@ -22,41 +29,54 @@ def get_arrow(theta,ys1,ys2,ys3):
     v = ys2#np.sin(3*theta)
     w = ys3#np.cos(3*theta)
     return x,y,z,u,v,w
+
+serv = Listener(('',5005))
+print("listening")
+client = serv.accept()
+#cli = Client(('localhost', 5000))
+print("bye")
+parsed_msg = [0,0,0]
+def update(theta,ys1,ys2,ys3):
+    global quiver, parsed_msg
+    quiver.remove()
+
+#ValueError: shape mismatch: objects cannot be broadcast to a single shape
+
+    if(client.poll(0)):
+        acceldata = client.recv()
+        print("Received data: ", acceldata)
+        parsed_msg=literal_eval(acceldata)
+        print(parsed_msg)
+
+    
+    quiver = ax.quiver(*get_arrow(theta,*parsed_msg)) 
+    # Limit x and y lists to 200 samples - 2s 
+
+    # Draw dis and accel lists
+    #line1.set_ydata(ys1)
+    #line2.set_ydata(ys2)
+    #line3.set_ydata(ys3)
+
+    #return line1,line2,line3,
+
+
+
+
+
 x_len = 20
 ys1 = [0] * x_len
 ys2 = [0] * x_len
 ys3 = [0] * x_len
 
-
+#create fig
+fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
+#quiver
 quiver = ax.quiver(*get_arrow(0,ys1,ys2,ys3))
-
+#params
 ax.set_xlim(-80, 80)
 ax.set_ylim(-80, 80)
 ax.set_zlim(-80, 80)
-
-def update(theta,ys1,ys2,ys3):
-    global quiver
-    quiver.remove()
-
-#TODO: (depends on format of data)fill in with appending accelero data
-#so append will just need to change based on that.
-    
-
-    ys1.append(30)
-    ys2.append(0)
-    ys3.append(-20)
-    quiver = ax.quiver(*get_arrow(theta,ys1,ys2,ys3)) 
-    # Limit x and y lists to 200 samples - 2s 
-    ys1 = ys1[-x_len:]
-    ys2 = ys2[-x_len:]
-    ys3 = ys3[-x_len:]
-
-    # Draw dis and accel lists
-    line1.set_ydata(ys1)
-    line2.set_ydata(ys2)
-    line3.set_ydata(ys3)
-
-    return line1,line2,line3,
-
 ani = FuncAnimation(fig, update, fargs=(ys1, ys2, ys3), frames=np.linspace(0,2*np.pi,200), interval=5) #increase rate of processing by lowering interval
-plt.show() #use fargs to update for the update and append those to update the uv w?
+plt.show()        
+
+
